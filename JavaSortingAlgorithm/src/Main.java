025f405c2d8438a78ef7df0f39d47848ac9fd68e
Giuseppe <giuse88@gmail.com>
2014-02-15 17:46:53 +0000

    Removed unused System.out.println()

--- JavaSortingAlgorithm/src/Main.java
+++ JavaSortingAlgorithm/src/Main.java
@@ -3,7 +3,5 @@ public class Main {
 
 	static public void main(String []  args) {
 		System.out.println("Hello world");
-		System.out.println("Second hello world");
-		System.out.println("Third hello world");
 	} 
 }
