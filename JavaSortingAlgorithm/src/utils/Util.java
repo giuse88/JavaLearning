83857622830230fb450092c3d41904680ae7deb0
Giuseppe <giuse88@gmail.com>
2014-02-17 23:17:08 +0000

    Added Util class with swap method for List

---
+++ JavaSortingAlgorithm/src/utils/Util.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.List;
+
+public class Util {
+
+	public static final <T extends Comparable<T>> void swap(List<T> list, int pos_a, int pos_b) {
+        T temp= list.get(pos_a);
+        list.set(pos_a, list.get(pos_b));
+        list.set(pos_b,temp);
+    }
+}
