e8996041d48d94d05be8e37c88c1c138f7e3e280
Giuseppe <giuse88@gmail.com>
2014-02-17 23:17:46 +0000

    Implemented QuickSort algorithm

--- JavaSortingAlgorithm/src/Main.java
+++ JavaSortingAlgorithm/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+
+import quickSort.QuickSort;
+import sortInterface.Sort;
+
 
 public class Main {
 	static public void main(String []  args) {
 		System.out.println("Java Learning algorithm");
+		
+		Sort sorting = new QuickSort();
+		
+		System.out.println(sorting.sort(Arrays.asList(new Integer[] {4,8,1,3,0,9,1})));
+
 	} 
 }
--- JavaSortingAlgorithm/src/quickSort/QuickSort.java
+++ JavaSortingAlgorithm/src/quickSort/QuickSort.java
@@ -1,12 +1,43 @@
 package quickSort;
 
-public class QuickSort<T> implements Comparable<T> {
+import java.util.List;
+import java.util.Random;
 
+import sortInterface.Sort;
+import utils.Util;
 
+public class QuickSort implements Sort {
+	
+	static final Random RND = new Random(); 
+	
+    private <T extends Comparable<T>> int partition(List<T> list, int begin, int end ){
+
+        int index = begin + RND.nextInt(end - begin + 1);
+        T pivot = list.get(index);
+        Util.swap(list, index, end);
+        for (int i = index = begin; i < end; ++ i) {
+            if (list.get(i).compareTo(pivot) < 0) {
+                Util.swap(list, index++, i);
+            }
+        }
+        Util.swap(list, index, end);
+        return (index);
+    }
+
+   private <T extends Comparable<T>> void _quickSort( List<T> array, int first, int last) {
+
+        if (first >= last )
+            return;
+
+        int pivot =  partition(array, first, last);
+        _quickSort(array, first, pivot - 1);
+        _quickSort(array, pivot + 1, last);
+    }
+	
 	@Override
-	public int compareTo(T o) {
-		return 0;
+	public <T extends Comparable<T>> List<T> sort(List<T> collection) {
+		_quickSort(collection, 0, collection.size() -1);
+		return collection;
 	}
 
-	
 }
