1a118ddbb45b38bac27e9e4305b7a5f913b68b73
Giuseppe <giuse88@gmail.com>
2014-02-22 21:53:09 +0000

    Added tests and ant build script

---
+++ JavaSortingAlgorithm/src/tests/TestSuite.java
@@ -0,0 +1,13 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSuite {
+	
+	@Test
+	public void ThisIsATest() {
+		assertTrue(true);
+	}
+}
