966385c80fe6e758505f89659c45ed6df79a55e1
Giuseppe <giuse88@gmail.com>
2014-03-01 12:59:15 +0000

    Fixed import in the Main class

--- JavaSortingAlgorithm/src/sorting/main/Main.java
+++ JavaSortingAlgorithm/src/sorting/main/Main.java
@@ -4,13 +4,12 @@ import java.util.Arrays;
 import sorting.algorithms.QuickSort;
 import sorting.interfaces.Sort;
 
-
 public class Main {
 	static public void main(String []  args) {
 		System.out.println("Java Learning algorithm");
 		
 		Sort sorting = new QuickSort();
-		
+
 		System.out.println(sorting.sort(Arrays.asList(new Integer[] {4,8,1,3,0,9,1})));
 
 	} 
