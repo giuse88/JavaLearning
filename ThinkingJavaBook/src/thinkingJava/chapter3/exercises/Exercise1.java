131b54a317d343100cbbcd1054ca4068a0ea167a
Giuseppe <giuse88@gmail.com>
2014-03-02 17:35:58 +0000

    Fix space in the output string exercise 1

--- ThinkingJavaBook/src/thinkingJava/chapter3/exercises/Exercise1.java
+++ ThinkingJavaBook/src/thinkingJava/chapter3/exercises/Exercise1.java
@@ -9,8 +9,8 @@ public class Exercise1 {
 		int localVariable = 0; 
 		// the fields should be automatically initialized to zero k
 		System.out.println("======== Exercise 1 ========");
-		System.out.println("Number : This should be zero " + thisIsANumber);
-		System.out.println("Char 	: This should be zero too " + thisIsAchar);
+		System.out.println("Number	: This should be zero " + thisIsANumber);
+		System.out.println("Char	: This should be zero too " + thisIsAchar);
 		System.out.println("Java does not allow not initialized variables  : "
 							+ localVariable );
 		System.out.println("============================");
