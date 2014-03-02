3123b6d18ddad6626746f78d70a7ecd96dfe297f
Giuseppe <giuse88@gmail.com>
2014-03-02 16:59:58 +0000

    Added Documentation 3 ( list html )

---
+++ ThinkingJavaBook/src/thinkingJava/chapter3/documentation/Documentation3.java
@@ -0,0 +1,15 @@
+package thinkingJava.chapter3.documentation;
+
+/** 
+ * You can <em> even </em> insert a list: 
+ * <ol> 
+ * 	<li> Item one </li> 
+ * 	<li> Item two </li> 
+ * 	<li> Item three </li> 
+ * </ol> 
+ * @author giuseppe
+ *
+ */
+public class Documentation3 {
+
+}
