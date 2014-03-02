62dcc8bb55b78f2309040132b5d9ccf6df8f516d
Giuseppe <giuse88@gmail.com>
2014-03-02 16:58:40 +0000

    Added Documentation 1 class \

---
+++ ThinkingJavaBook/src/thinkingJava/chapter3/documentation/Documentation1.java
@@ -0,0 +1,12 @@
+package thinkingJava.chapter3.documentation;
+
+/** A class comment */ 
+public class Documentation1 {
+	
+	/** A field comment */ 
+	public int i; 
+	
+	/** A method comment */ 
+	public void foo(){}; 
+
+}
