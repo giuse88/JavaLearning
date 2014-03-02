e97449ca8b2ed89339c62b52afdf6b9599690288
Giuseppe <giuse88@gmail.com>
2014-03-02 17:34:14 +0000

    Exercise 1 ( silly function )

---
+++ ThinkingJavaBook/src/thinkingJava/chapter3/exercises/Exercise1.java
@@ -0,0 +1,18 @@
+package thinkingJava.chapter3.exercises;
+
+public class Exercise1 {
+
+	int thisIsANumber; 
+	char thisIsAchar; 
+	
+	public void excerse1() {
+		int localVariable = 0; 
+		// the fields should be automatically initialized to zero k
+		System.out.println("======== Exercise 1 ========");
+		System.out.println("Number : This should be zero " + thisIsANumber);
+		System.out.println("Char 	: This should be zero too " + thisIsAchar);
+		System.out.println("Java does not allow not initialized variables  : "
+							+ localVariable );
+		System.out.println("============================");
+	}
+}
--- ThinkingJavaBook/src/thinkingJava/main/ThinkingJavaMain.java
+++ ThinkingJavaBook/src/thinkingJava/main/ThinkingJavaMain.java
@@ -1,14 +1,23 @@
 package thinkingJava.main;
 
 import thinkingJava.chapter3.ShowProperties;
+import thinkingJava.chapter3.exercises.Exercise1;
 
 public class ThinkingJavaMain {
 
 	public static void main(String[] args) {
 		System.out.println("Thinkin Java Application");
-		
+	
+		System.out.println(" ******* Chapter 3  *********");
+
 		// Page 80 Java Properties 
 		ShowProperties.printJavaProperties();
+
+		// Exercises : 
+		Exercise1 exercise = new Exercise1(); 
+		exercise.excerse1();
+
+		System.out.println(" ***************************");
 	}
 
 }
